b08bef42869fab3d60d00e03887dec573321e7da
KovalIllia <koval.illia.mailbox@gmail.com>
2025-09-18 22:26:36 +0300

    chapter_1 file 1.2

--- chapter_1/task_1.2.py
+++ chapter_1/task_1.2.py
@@ -1,8 +1,8 @@
 print("Enter 2 values")
-a=int(input("enter the first number: "))
-b=int(input("enter the second number: "))
-if b!=0:
-    c=a/b
+a = int(input("enter the first number: "))
+b = int(input("enter the second number: "))
+if b != 0:
+    c = a / b
     print(f"the result of dividing one number by another: {c}")
 else:
     print("no solution")
